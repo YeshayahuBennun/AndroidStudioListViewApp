1ad78cf7c430b2f79f171039838139a3826b5561
YeshayahuBennun <ybennun@yahoo.com>
2021-09-13 17:57:34 +0300

    commit#3 - Creating Data Model By data Class Using            ArrayList

---
+++ app/src/main/java/com/ybennun/listview/Data.kt
@@ -0,0 +1,5 @@
+package com.ybennun.listview
+
+data class Data(val number:String,val audioFileName:String)
+
+data class User(val name:String,val age:Int, val birthDate:Int)
--- app/src/main/java/com/ybennun/listview/MainActivity.kt
+++ app/src/main/java/com/ybennun/listview/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val array = arrayOf(
+        val array: Array<String> = arrayOf(
             "1",
             "2",
             "3",
@@ -35,5 +35,19 @@ class MainActivity : AppCompatActivity() {
 
         val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, array)
         list_view.adapter = adapter
+
+        val arrayList: ArrayList<Data> = ArrayList()
+        arrayList.add(Data("0", "audio_0"))
+        arrayList.add(Data("1", "audio_1"))
+        arrayList.add(Data("2", "audio_2"))
+        arrayList.add(Data("3", "audio_3"))
+        arrayList.add(Data("4", "audio_4"))
+        arrayList.add(Data("5", "audio_5"))
+        arrayList.add(Data("6", "audio_6"))
+        arrayList.add(Data("7", "audio_7"))
+        arrayList.add(Data("8", "audio_8"))
+        arrayList.add(Data("9", "audio_9"))
+        arrayList.add(Data("10", "audio_10"))
+
     }
 }
