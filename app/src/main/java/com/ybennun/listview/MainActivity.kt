08b0a3a00aa1e2276d537206e9139f7bbb5c70fc
YeshayahuBennun <ybennun@yahoo.com>
2021-09-13 11:00:51 +0300

    commit#2 - Create a Simple List Using ArrayAdapter

--- app/src/main/java/com/ybennun/listview/MainActivity.kt
+++ app/src/main/java/com/ybennun/listview/MainActivity.kt
@@ -2,10 +2,38 @@ package com.ybennun.listview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val array = arrayOf(
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20"
+        )
+
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, array)
+        list_view.adapter = adapter
     }
 }
