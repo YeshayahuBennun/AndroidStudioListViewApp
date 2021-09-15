06c149c19ec1d38452a152603e66bade9f8c5030
YeshayahuBennun <ybennun@yahoo.com>
2021-09-15 11:32:34 +0300

    commit#6 - Create a Media Player in a CustomListView

--- app/src/main/java/com/ybennun/listview/CustomAdaptor.kt
+++ app/src/main/java/com/ybennun/listview/CustomAdaptor.kt
@@ -1,10 +1,12 @@
 package com.ybennun.listview
 
 import android.content.Context
+import android.media.MediaPlayer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
 import android.widget.TextView
 
 class CustomAdaptor(var arrayList: ArrayList<Data>) : BaseAdapter() {
@@ -23,7 +25,20 @@ class CustomAdaptor(var arrayList: ArrayList<Data>) : BaseAdapter() {
         val item = arrayList[position]
 
         val numberTextView = rowView.findViewById<TextView>(R.id.number_text_view)
-        numberTextView.text= item.number
+        numberTextView.text = item.number
+
+        val audioImageView = rowView.findViewById<ImageView>(R.id.audio_image_view)
+        audioImageView.setOnClickListener {
+
+            val mediaPlayer =
+                MediaPlayer.create(
+                    context,
+                    context.resources.getIdentifier(item.audioFileName, "raw", context.packageName)
+                )
+
+            mediaPlayer.start()
+
+        }
 
         return rowView
     }
